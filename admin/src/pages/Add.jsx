44b88cedb8f1a807c3422f3a70fe4f64c50de744
Chi <hoangthilinhchi131@gmail.com>
2025-06-23 10:25:15 +0700

    add require

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -117,7 +117,7 @@ const Add = ({token}) => {
 
             <div>
               <p className='mb-2'>Product Price</p>
-              <input onChange={(e)=>setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' type="number" placeholder='25' />
+              <input onChange={(e)=>setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' type="number" placeholder='25' required />
             </div>
 
         </div>
